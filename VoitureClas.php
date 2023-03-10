f4f026aa5cd507247c4cd042d83210080fc2c84b
Charlydcn <charly.ducournau@outlook.fr>
2023-03-10 13:35:47 +0100

    done

--- VoitureClas.php
+++ VoitureClas.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
-    <title>PHP 2</title>
-</head>
-<body>
-
-    <?php
+<?php
     
     class VoitureClas
 
@@ -67,8 +56,5 @@
 
             }
     
-    ?>
+?>
 
-
-</body>
-</html>
--- VoitureElec.php
+++ VoitureElec.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
-    <title>PHP 2</title>
-</head>
-<body>
-
-    <?php
+<?php
     
     class VoitureElec extends VoitureClas
 
@@ -53,8 +42,4 @@
 
             }
     
-    ?>
-
-
-</body>
-</html>
+?>
